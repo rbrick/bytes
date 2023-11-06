c397eedd3b6181645f71055dc8d177ed268c6e32
Ryan W <rbrickproductions123@gmail.com>
2023-11-06 16:17:30 -0600

    begin fleshing out wallet model

--- models/responses.go
+++ models/responses.go
@@ -5,4 +5,18 @@ type Prices struct {
 	EtherPrice string `json:"etherPrice"`
 }
 
-type Wallet struct{}
+type Wallet struct {
+	// The balance of the 'BYTES' token
+	TokenBalance string `json:"balance"`
+	// The amount of 'BYTES' tokens yielded.
+	YieldedTokens string `json:"yield"`
+	// The citizens staked for this wallet
+	StakedCitizens []Citizen `json:"stakedCitizens"`
+}
+
+type Citizen struct {
+	// The ID of this citizen
+	ID int `json:"id"`
+	// The image for this citizen
+	Image string `json:"image"`
+}
