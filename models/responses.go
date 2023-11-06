72e57aec70a1eb71b1981a3827f494a410f9b674
Ryan W <rbrickproductions123@gmail.com>
2023-11-06 15:57:34 -0600

    restructure tests

--- models/responses.go
+++ models/responses.go
@@ -4,3 +4,5 @@ type Prices struct {
 	USDPrice   string `json:"usdPrice"`
 	EtherPrice string `json:"etherPrice"`
 }
+
+type Wallet struct{}
--- price/converter_test.go
+++ price/converter_test.go
@@ -1,4 +1,4 @@
-package tests
+package price
 
 import (
 	"math/big"
