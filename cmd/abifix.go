2f03c54b16164803ac9a05a7cc96495c35ef0fe0
Ryan W <rbrickproductions123@gmail.com>
2023-11-06 18:17:20 -0600

    delete unnecessary comments and files

--- cmd/abifix.go
+++ cmd/abifix.go
@@ -74,14 +74,4 @@ var abifixCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(abifixCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// abifixCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// abifixCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
